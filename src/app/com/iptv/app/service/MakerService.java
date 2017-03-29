9aec47ac31168007db5125991fce6d47a42d2564
mac <mac@liqp.local>
2017-03-29 09:37:10 +0800

    update

---
+++ src/app/com/iptv/app/service/MakerService.java
@@ -0,0 +1,20 @@
+package com.iptv.app.service;
+
+import java.util.Map;
+
+import com.iptv.core.common.BizException;
+import com.iptv.core.common.KendoResult;
+import com.iptv.core.service.BaseService;
+
+@SuppressWarnings("rawtypes")
+public interface MakerService extends BaseService{
+	public KendoResult getMakerPaged(Map map);
+	
+	public Map getMaker(Integer id);
+	
+	public void save(Map map) throws BizException;
+	
+	public void delete(Map map) throws BizException; 
+	
+	public KendoResult getAllMakers();
+}
